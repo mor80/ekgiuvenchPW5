95e230c77ad3258dd04b3cfece12236210fc5a49
mor80 <egmor80@gmail.com>
2025-01-11 18:17:49 +0300

    Initial Commit

---
+++ ekgiuvenchPW5Tests/ekgiuvenchPW5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ekgiuvenchPW5Tests.swift
+//  ekgiuvenchPW5Tests
+//
+//  Created by Emre Guvench on 11.01.2025.
+//
+
+import Testing
+@testable import ekgiuvenchPW5
+
+struct ekgiuvenchPW5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
